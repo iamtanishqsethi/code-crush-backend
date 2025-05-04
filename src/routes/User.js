2df0469832bdac813bc00b7e6c0ebaf725c8d8a6
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-05-04 14:43:04 +0530

    feat:feed api

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {userAuth} = require("../middlewares/auth")
 const ConnectionRequest=require("../models/ConnectionRequest.js")
+const User=require("../models/user.js")
 const router = express.Router();
 
 router.get('/requests',userAuth,async (req,res)=>{
@@ -34,6 +35,39 @@ router.get('/connections',userAuth,async (req,res)=>{
         })
 
         res.json({message:"Data fetched successfully.",data})
+    }
+    catch(error){
+        res.status(400).json({message:`${error}`});
+    }
+})
+
+router.get('/feed',userAuth,async (req,res)=>{
+    try{
+
+        const page=parseInt(req.query.page) || 1;
+        let limit=parseInt(req.query.limit) || 10;
+        limit=limit>50 ? 50 : limit;
+        const skip=(page-1)*limit;
+
+        const loggedInUser=req.user;
+        //all connection requests that the user has sent or received
+        const connectionRequests=await ConnectionRequest.find({
+            $or:[{toUserId:loggedInUser._id},{fromUserId:loggedInUser._id}]
+        }).select("fromUserId toUserId")
+
+        const hiddenUsers=new Set();
+        connectionRequests.forEach(req=>{
+            hiddenUsers.add(req.fromUserId.toString())
+            hiddenUsers.add(req.toUserId.toString())
+        })
+
+        const users=await User.find({
+            $and:[
+                {_id:{$nin:Array.from(hiddenUsers)}},
+                {_id:{$ne:loggedInUser._id}}
+            ]
+        }).select("firstName lastName photoUrl age gender about skills").skip(skip).limit(limit)
+        res.send(users)
     }
     catch(error){
         res.status(400).json({message:`${error}`});
