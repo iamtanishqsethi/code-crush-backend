23a3f5ef1e8e2dc33c591e50ca6e355b0cc9693b
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-05-11 13:12:03 +0530

    fix : cors issue

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ const UsersAuth=require('./routes/Auth.js')
 const Profile=require('./routes/Profile.js')
 const Request=require('./routes/Request.js')
 const User=require('./routes/User.js')
+const http = require('http')
+const socket=require("socket.io")
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://code--crush.vercel.app"],
     credentials: true,
 }))
 require('dotenv').config();
@@ -21,9 +23,21 @@ app.use('/api/profile',Profile)
 app.use('/api/request',Request)
 app.use('/api/user',User)
 
+const server=http.createServer(app);
+const io=socket(server,{
+    cors:{
+        origin:"http://localhost:5173",
+    }
+})
+
+io.on("connection",(socket)=>{
+    //handle events
+})
+
+
 connectDb().then(()=>{
     console.log("connected to database")
-    app.listen(7777, () => {
+    server.listen(7777, () => {
         console.log("Server is successfully listening on port 7777...");
       });
     
